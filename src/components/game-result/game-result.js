1034b8df5a9a26d9ed4deff149dbb4ea8668940e
Alexei Kravchuk <alexei.kravchuk21@gmail.com>
2020-08-19 14:57:16 +0300

    feat: implement absolut victory message

--- src/components/game-result/game-result.js
+++ src/components/game-result/game-result.js
@@ -4,6 +4,13 @@ import './game-result.scss';
 export const GameResult = ({ score, resetGame }) => {
   const maxScore = 30;
 
+  const resultText =
+    score === maxScore ? (
+      <span className='victory'>С абсолютной победой!!!</span>
+    ) : (
+      `Вы прошли викторину и набрали ${score} баллов из ${maxScore}`
+    );
+
   return (
     <div className="modal">
       <div className="modal-dialog" role="document">
@@ -12,7 +19,7 @@ export const GameResult = ({ score, resetGame }) => {
             <h2 className="modal-title text-success">Поздравляем!</h2>
           </div>
           <div className="modal-body">
-            <p className="text-center">{`Вы прошли викторину и набрали ${score} баллов из ${maxScore}`}</p>
+            <p className="text-center">{resultText}</p>
           </div>
           <div className="modal-footer">
             <button type="button" className="btn btn-lg btn-block btn-success" onClick={resetGame}>
